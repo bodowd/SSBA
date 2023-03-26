ee94fdf3a2c02117c1b13c315a48475e943b8784
bodowd <odowd.bing@gmail.com>
2023-03-26 17:21:01 +0200

    Add binary conversion solution
    Two passes. Because we iterate from left to right, but the place counter
    starts from 0 from right to left, we need to find the length of the
    string to know what order the highest order bit is.
    
    First pass counts the string length. We get our place counter here.
    Second pass checks if there is a 1 at each iteration, and if so
    calculates the decimal value of that place and adds it to the total.
    Then we iterate +1 in the byte array and -1 in the place counter.

--- 3_x86-64_Programming/binary_convert.asm
+++ 3_x86-64_Programming/binary_convert.asm
@@ -1,4 +1,31 @@
 section .text
 global binary_convert
 binary_convert:
+    mov rdx, -1  ; intialize place
+    mov rax, 0 ; initialize total
+    mov rsi, rdi ; copy rdi to rsi to do first iteration to get string length
+strlen:
+    cmp [rsi], byte 0 ; iterate through string and count the length
+    jz .L1 ; go to the next loop if we hit the end of the string
+    inc rdx ; increment the place counter
+    add rsi, 1 ; go to next element in the array
+    jmp strlen
+.L1:
+    cmp [rdi], byte 0 ; check if this element is the null byte -- end of string
+    jz done ; if null byte, exit
+    cmp [rdi], byte 49 ; check if the current element is equal to 49 the ascii code for '1'
+    jz .calc
+    jmp .next_iteration
+.calc:
+    mov rcx, rdx ; shift count must go in rcx, set it to current place
+    mov r10, 1 ; store the result of bit shift in r10. first set it to 1
+    shl r10, cl ; shift left to calculate 2 ** place
+    add rax, r10 ; add the current amount to the total
+    jmp .next_iteration
+.next_iteration:
+    dec rdx ; decrement place
+    add rdi, 1 ; next element in the array
+    jmp .L1 ; go to top of the loop
+
+done:
 	ret
