101988907a113dac4a00f8305980b83e1890920a
bodowd <odowd.bing@gmail.com>
2023-03-26 16:50:24 +0200

    Fix binary_convert C code

--- 3_x86-64_Programming/scrap/binary_convert.c
+++ 3_x86-64_Programming/scrap/binary_convert.c
@@ -5,23 +5,23 @@
 int binary_convert(char s[]);
 
 int binary_convert(char s[]) {
-  int i;
-  int place = 0;
+  int i = 0;
+  int place = strlen(s) - 1;
   int total = 0;
-  for (i = 0; i < strlen(s); i++) {
-    char s1 = s[i];
-    if (strcmp(&s1, "1")) {
+  while (s[i] != '\0') {
+    if (s[i] == '1') {
       // total += (int)pow(2, place);
       // calculate pow(2, place) with bit shift
       total += 1 << place;
     }
-    place += 1;
+    place -= 1;
+    i++;
   }
   return total;
 }
 
 int main() {
-  char s[20] = "1110";
+  char s[20] = "111111";
   printf("%d\n", binary_convert(s));
   return 0;
 }
